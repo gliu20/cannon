f781d30c49ad0482cbc629e58f83f85d184c337b
George <24921711+gliu20@users.noreply.github.com>
2022-01-07 22:42:44 +0000

    switch picture to img

--- src/components/picture.jsx
+++ src/components/picture.jsx
@@ -1,11 +1,10 @@
 import * as React from 'react'
-import { StaticImage } from "gatsby-plugin-image"
 import "../styles/picture.scss"
 
 const Picture = ({ src, alt, modifiers }) => {
     return (
         <div className={"picture " + (modifiers || '')}>
-            <StaticImage className="picture__img" alt={alt} src={src}></StaticImage>
+            <img className="picture__img" alt={alt} src={src}></img>
         </div>
     )
 }
