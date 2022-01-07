f41786a75f0e3fb2eb7f6d6c74513aa97ef56609
George <24921711+gliu20@users.noreply.github.com>
2022-01-07 22:43:38 +0000

    add placeholder to scss

--- src/components/picture.jsx
+++ src/components/picture.jsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import "../styles/picture.scss"
 
+// TODO later figure out adding
+// dynamic images with GatsbyImage for perf/SEO
+
 const Picture = ({ src, alt, modifiers }) => {
     return (
         <div className={"picture " + (modifiers || '')}>
