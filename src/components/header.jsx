d27ce6cfe3758368acd3b378b7b2b398e48b6dac
George <24921711+gliu20@users.noreply.github.com>
2022-01-07 14:35:03 +0000

    move styles into scss

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import "../styles/header.scss"
 
 const Header = () => {
     return (
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import Header from './header'
 import Footer from './footer'
+import "../styles/index.scss"
 
 const Layout = ({ pageTitle, children }) => {
   return (
