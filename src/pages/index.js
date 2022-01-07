2b86a5f57bda57808cfa9e0d55ef76f774185f26
George <24921711+gliu20@users.noreply.github.com>
2022-01-07 22:44:06 +0000

    consume data from wordpress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,129 +1,125 @@
 import * as React from "react"
+import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 import Section from "../components/section"
 import Card from "../components/card"
 import ArticleGallery from "../components/articleGallery"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+
+  const posts = data.allWpPost.edges;
+
+  const featuredPosts = posts.slice(0, 7);
+  const featuredPostsCenter = featuredPosts.slice(0, 1);
+  const featuredPostsLeft = featuredPosts.slice(1, 4);
+  const featuredPostsRight = featuredPosts.slice(4, 7);
+
+  const recentPosts = posts.slice(7,17);
+
   return (
     <Layout pageTitle="The Cannon Newspaper">
       <Section title="Featured">
         <ArticleGallery left={
           <div>
-            <Card
-              level={3}
-              title="Celebrating Occasions During COVID-19"
-              subtitle="By Kirtana Devaraj"
-              to="/"
-              modifiers="card--link"
-            ></Card>
-            <Card
-              level={3}
-              mediaSrc="../images/peep.jpg"
-              title="Not a Peep"
-              subtitle="By Andrew Wuebbolt"
-              to="/"
-              modifiers="card--link"
-            ></Card>
-            <Card
-              level={3}
-              title="COVID-19 and the Sustainable Engineers Association"
-              subtitle="By Adam Koa"
-              to="/"
-              modifiers="card--link"
-            ></Card>
+            {
+              featuredPostsLeft.map(post => <Card
+                level={3}
+                mediaSrc={post.node?.featuredImage?.node?.sourceUrl || true}
+                mediaAlt={post.node?.featuredImage?.node?.alt || post.node?.featuredImage?.node?.title}
+                mediaModifiers="picture--3by2"
+                title={post.node.title}
+                subtitle={`By ${post.node.author.node.name}`}
+                to={post.node.slug}
+                modifiers="card--link"
+              ></Card>)
+            }
           </div>
         }
 
           right={
             <div>
-              <Card
-                level={3}
-                title="Your Cicadian Rhythm and Online Learning"
-                subtitle="By Vanessa Elizabeth Ayoung-Chee"
-                to="/"
-                modifiers="card--link"
-              ></Card>
-              <Card
-                level={3}
-                mediaSrc="../images/ads.jpg"
-                title="Youtube's Most Annoying Ads Ranking, Finally Revealed"
-                subtitle="By Ruknoon Dinder"
-                to="/"
-                modifiers="card--link"
-              ></Card>
-              <Card
-                level={3}
-                title="Celebrating Occasions During COVID-19"
-                subtitle="By Kirtana Devaraj"
-                to="/"
-                modifiers="card--link"
-              ></Card>
+              {
+                featuredPostsRight.map(post => <Card
+                  level={3}
+                  mediaSrc={post.node?.featuredImage?.node?.sourceUrl || true}
+                  mediaAlt={post.node?.featuredImage?.node?.alt || post.node?.featuredImage?.node?.title}
+                  mediaModifiers="picture--3by2"
+                  title={post.node.title}
+                  subtitle={`By ${post.node.author.node.name}`}
+                  to={post.node.slug}
+                  modifiers="card--link"
+                ></Card>)
+              }
             </div>
           }
 
           center={
             <div>
-              <Card
-                level={3}
-                mediaSrc="../images/mars.jpg"
-                title="NASA's Perseverance Rover Landing"
-                description="Mars: a world so far and foreign, but when you look at it
-                  closer, you can imagine how the Earth looked before it got to its 
-                  current state, with no civilization anywhere to be found..."
-                subtitle="By Joel Majano"
-                to="/"
-                modifiers="card--link"
-              ></Card>
+              {
+                featuredPostsCenter.map(post => <Card
+                  level={3}
+                  mediaSrc={post.node?.featuredImage?.node?.sourceUrl || true}
+                  mediaAlt={post.node?.featuredImage?.node?.alt || post.node?.featuredImage?.node?.title}
+                  mediaModifiers="picture--3by2"
+                  title={post.node.title}
+                  subtitle={`By ${post.node.author.node.name}`}
+                  description={post.node.excerpt}
+                  to={post.node.slug}
+                  modifiers="card--link"
+                ></Card>)
+              }
             </div>
           }
 
         ></ArticleGallery>
       </Section>
       <Section title="Recent Articles">
-        <Card
-          level={3}
-          mediaSrc="../images/placeholder.jpg"
-          title="Your Cicadian Rhythm and Online Learning"
-          subtitle="By Vanessa Elizabeth Ayoung-Chee"
-          to="/"
-          modifiers="card--link card--horizontal"
-        ></Card>
-        <Card
-          level={3}
-          mediaSrc="../images/ads.jpg"
-          title="Youtube's Most Annoying Ads Ranking, Finally Revealed"
-          subtitle="By Ruknoon Dinder"
-          to="/"
-          modifiers="card--link card--horizontal"
-        ></Card>
-        <Card
-          level={3}
-          mediaSrc="../images/placeholder.jpg"
-          title="Celebrating Occasions During COVID-19"
-          subtitle="By Kirtana Devaraj"
-          to="/"
-          modifiers="card--link card--horizontal"
-        ></Card>
-        <Card
-          level={3}
-          mediaSrc="../images/placeholder.jpg"
-          title="Celebrating Occasions During COVID-19"
-          subtitle="By Kirtana Devaraj"
-          to="/"
-          modifiers="card--link card--horizontal"
-        ></Card>
-        <Card
-          level={3}
-          mediaSrc="../images/placeholder.jpg"
-          title="Celebrating Occasions During COVID-19"
-          subtitle="By Kirtana Devaraj"
-          to="/"
-          modifiers="card--link card--horizontal"
-        ></Card>
+        {
+          recentPosts.map(post => <Card
+            level={3}
+            mediaSrc={post.node?.featuredImage?.node?.sourceUrl || true}
+            mediaAlt={post.node?.featuredImage?.node?.alt || post.node?.featuredImage?.node?.title}
+            mediaModifiers="picture--3by2"
+            title={post.node.title}
+            subtitle={`By ${post.node.author.node.name}`}
+            to={post.node.slug}
+            modifiers="card--link card--horizontal"
+          ></Card>)
+        }
       </Section>
     </Layout >
   )
 }
+
+export const query = graphql`query contextQuery {
+  allWpPost {
+    edges {
+      node {
+        title
+        slug
+        excerpt
+        featuredImage {
+          node {
+            sourceUrl
+            title
+            altText
+          }
+        }
+        author {
+          node {
+            name
+          }
+        }
+        date
+        categories {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
