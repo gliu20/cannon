edb92da1b6166907dad7eebc49f31cf301f5efe7
George <24921711+gliu20@users.noreply.github.com>
2022-01-07 19:33:31 +0000

    add section styles

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -2,9 +2,13 @@ import * as React from 'react'
 
 import "../styles/section.scss"
 
-const Section = ({ children }) => {
+const Section = ({ title, children, description }) => {
     return (
         <section className="section">
+            <header className="section__header">
+                {title}
+                {description}
+            </header>
             <div className="section__content">
                 {children}
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,51 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import Section from "../components/section"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Hello">
-      <p>This is some content 1</p>
-      <p>This is some content 2</p>
-      <p>This is some content 3</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content</p>
-      <p>This is some content 8</p>
-      <p>This is some content 9</p>
-      <p>This is some content 10</p>
+      <Section title={
+        <h2 className="section__title">Highlights</h2>
+      }>
+        <p>This is some content 1</p>
+        <p>This is some content 2</p>
+        <p>This is some content 3</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content</p>
+        <p>This is some content 8</p>
+        <p>This is some content 9</p>
+        <p>This is some content 10</p>
+      </Section>
     </Layout>
   )
 }
