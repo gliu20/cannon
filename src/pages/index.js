f34565bad6ea2dbb2822ff3458abe1cba853398c
George <24921711+gliu20@users.noreply.github.com>
2022-01-07 17:57:45 +0000

    finish menu styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,45 @@ import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Hello"></Layout>
+    <Layout pageTitle="Hello">
+      <p>This is some content 1</p>
+      <p>This is some content 2</p>
+      <p>This is some content 3</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content</p>
+      <p>This is some content 8</p>
+      <p>This is some content 9</p>
+      <p>This is some content 10</p>
+    </Layout>
   )
 }
 
