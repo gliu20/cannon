99b9196f06ce0844b51eb968c2db14e563a35581
George <24921711+gliu20@users.noreply.github.com>
2022-01-07 17:43:15 +0000

    move menu styles

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,40 +3,35 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import "../styles/menu.scss"
 
+// TODO refactor out link data
+
 const Menu = () => {
     return (
-        <footer className="footer">
-            <div className="footer__content">
-                <div className="footer__about">
-                    <p className="footer__about-wordmark">The Cannon</p>
-                    <p className="footer__about-description">The official (serious) newspaper of the University of Toronto
-                        Engineering Society since 1978.</p>
-                    <div className="footer__about-socials">
-                        <a href="">insta</a>
-                        <a href="">fb</a>
-                    </div>
-                </div>
-                <div className="footer__nav">
-                    <div className="footer__links">
-                        <ul className="footer__links-column">
-                            <li><a className="button button--link" href="">About</a></li>
-                            <li><a className="button button--link" href="">Contact</a></li>
-                            <li><a className="button button--link" href="">Engineering Society</a></li>
-                        </ul>
-                        <ul className="footer__links-column">
-                            <li><a className="button button--link" href="">Past Issues</a></li>
-                            <li><a className="button button--link" href="">Skule&trade;</a></li>
-                            <li><a className="button button--link" href="">Student Life</a></li>
-                            <li><a className="button button--link" href="">Opinions</a></li>
-                            <li><a className="button button--link" href="">Gallery</a></li>
-                        </ul>
-                    </div>
-                    <div className="footer__logo">
-                        <StaticImage placeholder="none" src="../images/cannon-footer.png" alt=""></StaticImage>
-                    </div>
-                </div>
+        <nav class="menu">
+            <div class="menu__content">
+                <ul class="menu__list">
+                    <li class="menu__list-item">
+                        <a class="button button--text" href="category.html">Skule&trade;</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="button button--text" href="category.html">Student Life</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="button button--text" href="category.html">Opinions</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="button button--text" href="category.html">Gallery</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="button button--text" href="category.html">Issues</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="button button--text" href="category.html">About</a>
+                    </li>
+                </ul>
+                <button class="search">Search</button>
             </div>
-        </footer>
+        </nav>
     )
 }
 
