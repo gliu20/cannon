56b04ffce43c9daaa9880ea99ad15522164b886b
George <24921711+gliu20@users.noreply.github.com>
2022-01-07 20:07:37 +0000

    update card with SEO for headings

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,18 +1,52 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 import "../styles/card.scss"
 
-const Card = ({ title, children, description }) => {
+const Card = ({ level, title, subtitle, media, to, children }) => {
+    let titleEle;
+
+    switch (level) {
+        case 1:
+            titleEle = <h1 className="card__title">{title}</h1>;
+            break;
+        case 2:
+            titleEle = <h2 className="card__title">{title}</h2>;
+            break;
+        case 3:
+            titleEle = <h3 className="card__title">{title}</h3>;
+            break;
+        case 4:
+            titleEle = <h4 className="card__title">{title}</h4>;
+            break;
+        case 5:
+            titleEle = <h5 className="card__title">{title}</h5>;
+            break;
+        case 6:
+            titleEle = <h6 className="card__title">{title}</h6>;
+            break;
+        default:
+            titleEle = <p className="card__title">{title}</p>;
+            break;
+    }
+
     return (
-        <article className="card">
-            <header className="card__header">
-                {title}
-                {description}
-            </header>
-            <div className="card__content">
-                {children}
-            </div>
-        </article>
+        <Link to={to} className="card__wrapper">
+            <article className="card card--link">
+                {media}
+                {
+                    title || subtitle ?
+                        <header className="card__header">
+                            {titleEle}
+                            {subtitle ? <p className="card__subtitle">{subtitle}</p> : ''}
+                        </header> : ''
+                }
+                <div className="card__content">
+                    {children}
+                </div>
+            </article>
+
+        </Link>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,20 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Section from "../components/section"
+import Card from "../components/card"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Hello">
       <Section title={
-        <h2 className="section__title">Highlights</h2>
+        <h2 className="section__title">Top Highlights</h2>
       }>
+        <Card
+          level={3}
+          title="Celebrating Occasions During COVID-19"
+          subtitle="By Kirtana Devaraj"
+        ></Card>
         <p>This is some content 1</p>
-        <p>This is some content 2</p>
-        <p>This is some content 3</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content</p>
-        <p>This is some content 8</p>
-        <p>This is some content 9</p>
         <p>This is some content 10</p>
       </Section>
     </Layout>
