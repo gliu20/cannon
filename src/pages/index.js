6e029f6ce41e627f64ab467e62bcebc21d3b99b6
George <24921711+gliu20@users.noreply.github.com>
2022-01-07 20:41:04 +0000

    add article gallery to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,32 +2,91 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Section from "../components/section"
 import Card from "../components/card"
+import ArticleGallery from "../components/articleGallery"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Hello">
       <Section title="Top Highlights">
-        <Card
-          level={3}
-          title="Celebrating Occasions During COVID-19"
-          subtitle="By Kirtana Devaraj"
-          to="/"
-          modifiers="card--link"
-        ></Card>
-        <Card
-          level={3}
-          title="Celebrating Occasions During COVID-19"
-          subtitle="By Kirtana Devaraj"
-          to="/"
-          modifiers="card--link"
-        ></Card>
-        <Card
-          level={3}
-          title="Celebrating Occasions During COVID-19"
-          subtitle="By Kirtana Devaraj"
-          to="/"
-          modifiers="card--link"
-        ></Card>
+        <ArticleGallery left={
+          <div>
+            <Card
+              level={3}
+              title="Celebrating Occasions During COVID-19"
+              subtitle="By Kirtana Devaraj"
+              to="/"
+              modifiers="card--link"
+            ></Card>
+            <Card
+              level={3}
+              title="Celebrating Occasions During COVID-19"
+              subtitle="By Kirtana Devaraj"
+              to="/"
+              modifiers="card--link"
+            ></Card>
+            <Card
+              level={3}
+              title="Celebrating Occasions During COVID-19"
+              subtitle="By Kirtana Devaraj"
+              to="/"
+              modifiers="card--link"
+            ></Card>
+          </div>
+        }
+        
+        right={
+          <div>
+            <Card
+              level={3}
+              title="Celebrating Occasions During COVID-19"
+              subtitle="By Kirtana Devaraj"
+              to="/"
+              modifiers="card--link"
+            ></Card>
+            <Card
+              level={3}
+              title="Celebrating Occasions During COVID-19"
+              subtitle="By Kirtana Devaraj"
+              to="/"
+              modifiers="card--link"
+            ></Card>
+            <Card
+              level={3}
+              title="Celebrating Occasions During COVID-19"
+              subtitle="By Kirtana Devaraj"
+              to="/"
+              modifiers="card--link"
+            ></Card>
+          </div>
+        }
+
+        center={
+          <div>
+            <Card
+              level={3}
+              title="Celebrating Occasions During COVID-19"
+              subtitle="By Kirtana Devaraj"
+              to="/"
+              modifiers="card--link"
+            ></Card>
+            <Card
+              level={3}
+              title="Celebrating Occasions During COVID-19"
+              subtitle="By Kirtana Devaraj"
+              to="/"
+              modifiers="card--link"
+            ></Card>
+            <Card
+              level={3}
+              title="Celebrating Occasions During COVID-19"
+              subtitle="By Kirtana Devaraj"
+              to="/"
+              modifiers="card--link"
+            ></Card>
+          </div>
+        }
+        
+        ></ArticleGallery>
       </Section>
     </Layout >
   )
