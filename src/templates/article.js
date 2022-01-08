1c1c5bbfd3a0f9ee9a9d7c9b5f0869bc82cd0e1b
George <24921711+gliu20@users.noreply.github.com>
2022-01-08 22:20:32 +0000

    prettify article content

--- src/templates/article.js
+++ src/templates/article.js
@@ -2,16 +2,29 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Section from "../components/section"
+import Picture from "../components/picture"
 
 const Article = ({ data }) => {
-    return (
-        <Layout pageTitle={data.wpPost.title}>
-            <Section>
-              {data.wpPost.title}
-              {data.wpPost.content}
-            </Section>
-        </Layout>
-    )
+  return (
+    <Layout pageTitle={data.wpPost.title}>
+      <Section>
+        <Picture src={data.wpPost?.featuredImage?.node?.sourceUrl}
+          modifiers="picture--3by2"
+        ></Picture>
+        <p dangerouslySetInnerHTML={({
+          '__html': data.wpPost?.featuredImage?.node?.caption
+        })}></p>
+      </Section>
+      <Section>
+        <header className="article__header">
+          <h1>{data.wpPost.title}</h1>
+        </header>
+        <div className="article__content" dangerouslySetInnerHTML={({
+          '__html': data.wpPost.content
+        })}></div>
+      </Section>
+    </Layout>
+  )
 }
 
 export default Article
@@ -24,6 +37,12 @@ export const query = graphql`
       id
       title
       content
+      featuredImage {
+        node {
+          caption
+          sourceUrl
+        }
+      }
     }
   }
 `
