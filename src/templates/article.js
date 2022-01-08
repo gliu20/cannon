55862ab55b9069504b709b71630fe97dbcff0fb3
George <24921711+gliu20@users.noreply.github.com>
2022-01-08 05:02:13 +0000

    add basic article template

---
+++ src/templates/article.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import Section from "../components/section"
+
+const Article = () => {
+    return (
+        <Layout pageTitle="TODO Post">
+            <Section>TODO</Section>
+        </Layout>
+    )
+}
+
+export default Article
