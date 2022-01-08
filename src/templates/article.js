cda143e5847a186628a9b8c0485d10193e7dc3e7
George <24921711+gliu20@users.noreply.github.com>
2022-01-08 17:37:34 +0000

    add graphql import wordpress for page creation

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,13 +1,29 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Section from "../components/section"
 
-const Article = () => {
+const Article = ({ data }) => {
     return (
-        <Layout pageTitle="TODO Post">
-            <Section>TODO</Section>
+        <Layout pageTitle={data.wpPost.title}>
+            <Section>
+              {data.wpPost.title}
+              {data.wpPost.content}
+            </Section>
         </Layout>
     )
 }
 
 export default Article
+
+// gatsby is smart enough to get post based on
+// id passed into this.props.pageContext
+export const query = graphql`
+  query($id: String) {
+    wpPost(id: { eq: $id }) {
+      id
+      title
+      content
+    }
+  }
+`
