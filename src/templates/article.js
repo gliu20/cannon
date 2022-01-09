9a4e180d4dfdf958d073fd92783390fa365d22f0
George <24921711+gliu20@users.noreply.github.com>
2022-01-09 01:05:27 +0000

    add article cards for see also section

--- src/templates/article.js
+++ src/templates/article.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import Section from "../components/section"
 import Picture from "../components/picture"
 import Tag from "../components/tag"
+import Card from "../components/card"
 
 import "../styles/article.scss"
 
@@ -39,7 +40,27 @@ const Article = ({ data }) => {
               //<Section renderAs="aside" title="Share"></Section>
             }
             <Section renderAs="aside" title="See also">
-              
+              <Card
+                level={3}
+                title="Stasis Theory and How to Be Persuasive"
+                subtitle="By Aristotle Philosopher"
+                toSlug="/"
+                modifiers="card--link"
+              ></Card>
+              <Card
+                level={3}
+                title="What Happens to Long Text on a Post Snippet?"
+                subtitle="By Concerned Software Tester"
+                toSlug="/"
+                modifiers="card--link"
+              ></Card>
+              <Card
+                level={3}
+                title="When Your Dog is Better Than You"
+                subtitle="By Non-Dog Owner"
+                toSlug="/"
+                modifiers="card--link"
+              ></Card>
             </Section>
           </div>
         </div>
